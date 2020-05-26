31950271b0410a85fd78bd75626f52c87424193a
Henrique Bissoli <emp.shad@gmail.com>
2020-05-26 19:37:17 -0300

    Install setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Download app
+
+bash setup.sh
+
+# Install containers
+
+docker-compose up -d
---
+++ setup.sh
@@ -0,0 +1,3 @@
+git clone git@github.com:Shadowz3n/admin-ui.git ui
+git clone git@github.com:Shadowz3n/admin-api.git api
+mkdir -p ./data/postgres
